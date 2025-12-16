132f60dca19d9a4ee353f9031eb2caec5bf10eca
arpamuji <aramdhan.pamuji@gmail.com>
2025-12-16 19:30:18 +0700

    style(ui): add Tailwind CSS v4 theme and Google Fonts
    Configures modern design system with Tailwind CSS v4 custom properties for colors, typography, and design tokens.
    Integrates Google Fonts (Geist, Fira Code, Merriweather) with preconnect optimization and subpixel antialiasing.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,9 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Geist:wght@100..900&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap"
+        rel="stylesheet">
 </head>
 
-<body>
+<body class="subpixel-antialiased">
     @inertia
 </body>
 
